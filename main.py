0e0589267c468c8fbc3895b5509fb601c1e5704a
Oscar <oscarpa_2000@hotmail.com>
2022-09-28 08:32:09 -0500

    reading the data

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 import pandas as pd
 
+# Import the data into a pandas dataframe
+path = 'Data/'
+dataframe = []
+for t in range(15, 46):
+    for g in range(400, 1000, 20):
+        filename = f'OutputPV_T{t}_G{g}.txt'
+        df = pd.read_csv(path + filename, header=None)
+        header = ['V', 'I']
+        df.columns = header
+        df['P'] = df['V'] * df['I']
+        df['T'] = ""
+        df['G'] = ""
+        df = df.assign(T=t, G=g)
+        dataframe.append(df)
+DF = pd.concat(dataframe, ignore_index=True)
+print(DF)
